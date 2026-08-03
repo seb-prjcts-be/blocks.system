0390db16357ed7910a1077f74d2cacfcb8aca7b9
Seb <108084902+seb-prjcts-be@users.noreply.github.com>
2026-08-03 17:50:13 +0200

    Refine home title styling

--- docs/home.mjs
+++ docs/home.mjs
@@ -8,7 +8,7 @@ blocks.setGrid(6, 8);
 blocks.snap = true;
 blocks.draggable = true;
 
-const title = blocks.add('<h1 class="home-title">blocks.system</h1>', { id: "home-title" });
+const title = blocks.add('<h1 class="home-title">Blocks. System.</h1>', { id: "home-title" });
 title.menu("blocks.system");
 title.span(3, 3);
 title.place(2, 3);
